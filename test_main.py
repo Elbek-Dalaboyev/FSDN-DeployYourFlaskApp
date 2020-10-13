b0a54ebd8c9d40c2a9df15f14b4dabf60f6be8cd
Elbek-Dalaboyev <70895732+Elbek-Dalaboyev@users.noreply.github.com>
2020-10-13 21:43:22 +0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,10 +7,10 @@ import pytest
 
 import main
 
-SECRET = 'TestSecret'
+SECRET = 'myjwtsecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-EMAIL = 'wolf@thedoor.com'
-PASSWORD = 'huff-puff'
+EMAIL = 'dalaboyeve@gmail.com'
+PASSWORD = 'believer_0'
 
 @pytest.fixture
 def client():
